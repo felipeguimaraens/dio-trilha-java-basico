f1bbe2f4d0ead377e9869b3bb425f9bdcf3214c7
felipeguimaraens <felipeguimaraens@hotmail.com>
2024-05-23 12:50:10 -0300

    Desafio contador

---
+++ Contador/DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+import java.lang.Exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message)
+        {
+            super(message);
+        }
+}
